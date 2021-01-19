e85d1a2561c4f172b20c1fa339544d673503b5f2
Michael Helfenstein <michael.helfenstein@mbc-engineering.ch>
2021-01-19 15:09:30 +0100

    fix projekt template

--- Source/MongoDbGridFsProvider/PostBuild.ps1
+++ Source/MongoDbGridFsProvider/PostBuild.ps1
@@ -1,10 +1,16 @@
 ﻿param (
 	[string]$Targetpath,
-	[string]$TargetDir
+	[string]$TargetDir,
+	[string]$TargetName
 )
+
+<#
+This script updated the Powershell Module Manifest to the actual module assembly version.
+#>
 
 Write-Host "Get Assembly version $TargetPath"
 $version = Get-Item $TargetPath | % versioninfo | % FileVersion
 
-Write-Host "Update Module Manifest with Assembly version $version"
-Update-ModuleManifest $TargetDir\MongoDbGridFsProvider.psd1 -ModuleVersion $version
+$manifestPath = "$TargetDir$TargetName.psd1"
+Write-Host "Update Module Manifest '$manifestPath' with Assembly version $version"
+Update-ModuleManifest $manifestPath -ModuleVersion $version
